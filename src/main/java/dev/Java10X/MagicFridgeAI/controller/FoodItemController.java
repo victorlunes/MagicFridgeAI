51e9f0cbf03fc5606405ae450ee58c51ba7cef58
victorlunes <victorllunes@gmail.com>
2026-05-28 21:55:22 -0300

    Feat: Created search by ID, update and delete

--- src/main/java/dev/Java10X/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/Java10X/MagicFridgeAI/controller/FoodItemController.java
@@ -1,7 +1,6 @@
 package dev.Java10X.MagicFridgeAI.controller;
 
 import dev.Java10X.MagicFridgeAI.DTO.FoodItemDTO;
-import dev.Java10X.MagicFridgeAI.FoodItemMapper.FoodItemMapper;
 import dev.Java10X.MagicFridgeAI.service.FoodItemService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,18 +18,37 @@ public class FoodItemController {
     }
 
     @GetMapping("/search-food")
-    public ResponseEntity<List<FoodItemDTO>> getAllFood(){
+    public ResponseEntity<List<FoodItemDTO>> getAllFood() {
         List<FoodItemDTO> foodItem = foodItemService.getFoodItem();
+        if(foodItem.isEmpty()) return ResponseEntity.status(HttpStatus.OK).body(null);
         return ResponseEntity.status(HttpStatus.OK).body(foodItem);
     }
 
+    @GetMapping("/search-food/{id}")
+    public ResponseEntity<FoodItemDTO> getFoodById(@PathVariable Long id) {
+        FoodItemDTO food = foodItemService.getById(id);
+
+        if(food == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+        return ResponseEntity.status(HttpStatus.OK).body(food);
+    }
+
     @PostMapping("/create-food")
-    public ResponseEntity<FoodItemDTO> createFood(@RequestBody FoodItemDTO foodItemDTO){
+    public ResponseEntity<FoodItemDTO> createFood(@RequestBody FoodItemDTO foodItemDTO) {
         FoodItemDTO foodItem = foodItemService.createFoodItem(foodItemDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(foodItem);
     }
 
-    //update
+    @PutMapping("/update-food/{id}")
+    public ResponseEntity<FoodItemDTO> updateFood(@PathVariable Long id, @RequestBody FoodItemDTO foodItemDTO) {
+        FoodItemDTO foodItem = foodItemService.updateFoodItem(id, foodItemDTO);
+        if(foodItem == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 
-    //delete
+        return ResponseEntity.status(HttpStatus.OK).body(foodItem);
+    }
+
+    @DeleteMapping("/delete-food/{id}")
+    public void deleteFood(@PathVariable Long id) {
+        foodItemService.deleteFoodItem(id);
+    }
 }
--- src/main/java/dev/Java10X/MagicFridgeAI/service/FoodItemService.java
+++ src/main/java/dev/Java10X/MagicFridgeAI/service/FoodItemService.java
@@ -7,6 +7,7 @@ import dev.Java10X.MagicFridgeAI.repository.FoodItemRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FoodItemService {
@@ -20,12 +21,36 @@ public class FoodItemService {
 
     public List<FoodItemDTO> getFoodItem() {
         List<FoodItemModel> foodItems = foodItemRepository.findAll();
+
         return foodItems.stream().map(food -> foodItemMapper.mapToDto(food)).toList();
+    }
+
+    public FoodItemDTO getById(Long Id){
+        Optional<FoodItemModel> foodItem = foodItemRepository.findById(Id);
+        return foodItem.map(food -> foodItemMapper.mapToDto(food)).orElse(null);
     }
 
     public FoodItemDTO createFoodItem(FoodItemDTO foodItemDTO) {
         FoodItemModel food = foodItemMapper.mapToModel(foodItemDTO);
         foodItemRepository.save(food);
         return foodItemMapper.mapToDto(food);
+    }
+
+    public FoodItemDTO updateFoodItem(Long id, FoodItemDTO newFoodItem){
+        FoodItemModel foodItem = foodItemRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Food not found !"));
+
+        foodItem.setNome(newFoodItem.getNome());
+        foodItem.setValidade(newFoodItem.getValidade());
+        foodItem.setCategoria(newFoodItem.getCategoria());
+        foodItem.setQuantidade(newFoodItem.getQuantidade());
+
+        FoodItemModel updatedFoodItem = foodItemRepository.save(foodItem);
+
+        return foodItemMapper.mapToDto(updatedFoodItem);
+    }
+
+    public void deleteFoodItem(Long id) {
+        foodItemRepository.deleteById(id);
     }
 }
