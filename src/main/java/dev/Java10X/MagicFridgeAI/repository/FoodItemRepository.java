0160b122dd519e9367c77b0a2455a75a549e5004
victorlunes <victorllunes@gmail.com>
2026-06-03 23:26:22 -0300

    refactor: move files to new directories

--- src/main/java/dev/Java10X/MagicFridgeAI/repository/FoodItemRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.Java10X.MagicFridgeAI.repository;
-
-import dev.Java10X.MagicFridgeAI.model.FoodItemModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FoodItemRepository extends JpaRepository<FoodItemModel, Long> {
-}
